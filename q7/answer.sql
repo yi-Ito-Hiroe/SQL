d264c19f2647c0efd821aa7be251c07a5d7f3bfe
Itou Hiroe <ito_hiroe@y-i-group.co.jp>
2025-05-28 21:02:56 +0900

    [add]:q7追加

---
+++ q7/answer.sql
@@ -0,0 +1,8 @@
+-- SQL課題⑦：最も書籍数の多い著者の書籍を取得（サブクエリ）
+SELECT * FROM books
+WHERE author_id = (
+    SELECT author_id FROM books
+    GROUP BY author_id             -- 著者ごとにグループ化
+    ORDER BY COUNT(*) DESC         -- 書籍の多い順に並び替え
+    LIMIT 1                        -- 1番多い著者だけを取得
+);
