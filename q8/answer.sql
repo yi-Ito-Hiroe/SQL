5444d29f26bdd06d66691471cdbde55f58eec9c3
Itou Hiroe <ito_hiroe@y-i-group.co.jp>
2025-05-28 21:23:01 +0900

    [add]:q8追加

---
+++ q8/answer.sql
@@ -0,0 +1,9 @@
+-- SQL課題⑧：価格帯で書籍を分類（CASE文）
+SELECT title, price,
+ CASE 
+    WHEN price <= 1500 THEN '安い'
+    WHEN price >= 1501 AND price <= 2500 THEN '普通'
+    ELSE '高い'
+ END AS price_category
+FROM books;
+
