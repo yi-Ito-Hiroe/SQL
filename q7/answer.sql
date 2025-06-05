3d98035018dec2a8b41139ba694271b7d6a1f212
Itou Hiroe <ito_hiroe@y-i-group.co.jp>
2025-06-05 20:31:13 +0900

    fix:Q7サブクエリに著者名のJOINを追加

--- q7/answer.sql
+++ q7/answer.sql
@@ -1,5 +1,7 @@
 -- SQL課題⑦：最も書籍数の多い著者の書籍を取得（サブクエリ）
-SELECT * FROM books
+SELECT b.*, a.name
+FROM books b
+JOIN authors a ON b.author_id = a.id
 WHERE author_id = (
     SELECT author_id FROM books
     GROUP BY author_id             -- 著者ごとにグループ化
